772df71e8ded2674d8df45e040e35295e7991c24
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2017-07-31 14:52:42 +0200

    feat: initial implementation

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,7 +1,57 @@
-import myModule from '..';
+import convertToJcamp from '..';
+import {convert} from 'jcampconverter';
 
-describe('test myModule', () => {
-    it('should return 42', () => {
-        expect(myModule()).toEqual(42);
+const testData = `1 2
+2 3
+3 4
+4 5
+5 6
+6 7
+7 8
+8 9`;
+
+describe('convertToJcamp', () => {
+    it('check valid with jcampconverter', () => {
+        var options = {
+            title: 'test',
+            owner: 'cheminfo',
+            origin: 'manually',
+            type: 'MASS SPECTRUM',
+            xUnit: 'M/Z',
+            yUnit: 'relative abundance',
+            info: {
+                info1: 'value1',
+                info2: 'value2'
+            }
+        };
+        var jcamp = convertToJcamp(testData, options);
+        var jcampObject = convert(jcamp);
+
+        expect(jcampObject.spectra).toEqual([
+            {
+                data: [[
+                    1, 2,
+                    2, 3,
+                    3, 4,
+                    4, 5,
+                    5, 6,
+                    6, 7,
+                    7, 8,
+                    8, 9
+                ]],
+                dataType: 'MASS SPECTRUM',
+                firstX: 1,
+                firstY: 2,
+                isPeaktable: true,
+                lastX: 8,
+                lastY: 9,
+                nbPoints: 8,
+                title: 'test',
+                xFactor: 1,
+                xUnit: 'M/Z',
+                yFactor: 1,
+                yUnit: 'relative abundance'
+            }
+        ]);
     });
 });
