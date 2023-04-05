3301f118210f924d92d2f82965316d17d8c24c00
jobo322 <jose.bolanos@correounivalle.edu.co>
2023-04-05 03:24:39 -0500

    fix: avoid reverse several times

--- src/from1DNMRVariables.ts
+++ src/from1DNMRVariables.ts
@@ -75,7 +75,7 @@ export function from1DNMRVariables(
     origin = '',
     dataType = 'NMR SPECTRUM',
     nucleus = info['.OBSERVE NUCLEUS'],
-    observeFrequency = info['.OBSERVE FREQUENCY'],
+    originFrequency = info['.OBSERVE FREQUENCY'],
   } = info;
 
   if (isRealData(variables)) {
@@ -85,55 +85,54 @@ export function from1DNMRVariables(
     );
   }
 
-  if (!observeFrequency) {
+  if (!originFrequency) {
     throw new Error(
       '.OBSERVE FREQUENCY is mandatory into the info object for nmr data',
     );
   }
 
-  if (!info['.OBSERVE FREQUENCY']) {
-    info['.OBSERVE FREQUENCY'] = observeFrequency;
+  const newInfo = {
+    '.OBSERVE FREQUENCY': originFrequency,
+    '.OBSERVE NUCLEUS': nucleus,
+    ...info,
+  };
+
+  const newVariables = checkDescending(variables);
+  const xVariable = newVariables.x as MeasurementVariable<DoubleArray>;
+
+  let xData = xVariable.data;
+  if (xVariable.units?.toLowerCase() !== 'hz') {
+    xData = xMultiply(xData, originFrequency);
   }
-  if (!info['.OBSERVE NUCLEUS']) info['.OBSERVE NUCLEUS'] = nucleus;
 
-  const xVariable = variables.x as MeasurementVariable<DoubleArray>;
-  const reverse = xVariable.data[0] < xVariable.data[1];
-
-  if (reverse) xVariable.data.reverse();
-  if (variables.x?.units?.toLowerCase() !== 'hz') {
-    xVariable.data = xMultiply(xVariable.data, observeFrequency);
-  }
-
-  const nbPoints = xVariable.data.length;
-  const spectralWidth = xVariable.data[0] - xVariable.data[nbPoints - 1];
+  const nbPoints = xData.length;
+  const spectralWidth = xData[0] - xData[nbPoints - 1];
   const symbol = ['X'];
-  const varName = [variables.x?.label.replace(/ *\[.*/, '') || 'X'];
+  const varName = [xVariable.label.replace(/ *\[.*/, '') || 'X'];
   const varType = ['INDEPENDENT'];
-  const varDim = [xVariable.data.length];
+  const varDim = [xData.length];
   const units = ['Hz'];
   const first = [spectralWidth];
   const last = [0];
   const min = [0];
   const max = [spectralWidth];
-  const factorArray = [xVariable.data.length / spectralWidth];
+  const factorArray = [xData.length / spectralWidth];
 
-  const keys = isPeakData(variables) ? peakDataKeys : ntuplesKeys;
+  const keys = isPeakData(newVariables) ? peakDataKeys : ntuplesKeys;
 
   for (const key of keys) {
-    let variable = variables[key];
+    let variable = newVariables[key];
 
     if (!variable) {
       if (key !== 'i') {
         throw new Error(
           `variable ${key} is mandatory in ${
-            isPeakData(variables) ? 'peak' : 'real/imaginary'
+            isPeakData(newVariables) ? 'peak' : 'real/imaginary'
           } data`,
         );
       }
       continue;
     }
-
-    if (reverse) variable.data.reverse();
 
     let name = variable?.label.replace(/ *\[.*/, '');
     let unit = variable?.label.replace(/.*\[(?<units>.*)\].*/, '$<units>');
@@ -165,11 +164,11 @@ export function from1DNMRVariables(
 ##ORIGIN=${origin}
 ##OWNER=${owner}\n`;
 
-  const infoKeys = Object.keys(info).filter(
+  const infoKeys = Object.keys(newInfo).filter(
     (e) =>
       !['title', 'owner', 'origin', 'datatype'].includes(e.toLocaleLowerCase()),
   );
-  header += addInfoData(info, infoKeys, '##');
+  header += addInfoData(newInfo, infoKeys, '##');
   header += addInfoData(meta);
 
   header += `##NTUPLES= ${dataType}
@@ -183,17 +182,17 @@ export function from1DNMRVariables(
 ##MAX=       ${max.join()}
 ##MIN=       ${min.join()}\n`;
 
-  if (isPeakData(variables)) {
-    let xData = variables.x.data;
-    let yData = variables.y.data;
+  if (isPeakData(newVariables)) {
+    let xData = newVariables.x.data;
+    let yData = newVariables.y.data;
     checkNumberOrArray(yData);
     header += `##DATA TABLE= (XY..XY), PEAKS\n`;
     for (let point = 0; point < varDim[0]; point++) {
       header += `${xData[point]}, ${yData[point]}\n`;
     }
-  } else if (isNTuplesData(variables)) {
+  } else if (isNTuplesData(newVariables)) {
     for (const key of ['r', 'i'] as const) {
-      const variable = variables[key];
+      const variable = newVariables[key];
 
       if (!variable) continue;
 
@@ -205,7 +204,7 @@ export function from1DNMRVariables(
       })), XYDATA\n`;
       header += vectorEncoder(
         rescaleAndEnsureInteger(variable.data, factor[key]),
-        xVariable.data.length - 1,
+        xData.length - 1,
         -1,
         xyEncoding,
       );
@@ -216,4 +215,21 @@ export function from1DNMRVariables(
   header += `##END NTUPLES= ${dataType}\n`;
   header += '##END=';
   return header;
+}
+
+function checkDescending(variables: NMR1DVariables) {
+  const xVariable = variables.x as MeasurementVariable<DoubleArray>;
+  const reverse = xVariable.data[0] < xVariable.data[1];
+
+  if (!reverse) return variables;
+
+  const newVariables = JSON.parse(
+    JSON.stringify(variables, (key, value) =>
+      ArrayBuffer.isView(value) ? Array.from(value as any) : value,
+    ),
+  );
+  for (const key of ['x', 'r', 'i'] as Array<keyof NtuplesData>) {
+    newVariables[key]?.data.reverse();
+  }
+  return newVariables;
 }
