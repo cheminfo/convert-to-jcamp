a3fc4a545a8a4aa93ea9e116559b09dfe2c744e3
jobo322 <jose.bolanos@correounivalle.edu.co>
2023-08-28 19:11:17 -0500

    fix: ensure integer in rescaling

--- src/utils/rescaleAndEnsureInteger.ts
+++ src/utils/rescaleAndEnsureInteger.ts
@@ -3,5 +3,5 @@ import { xDivide } from 'ml-spectra-processing';
 
 export function rescaleAndEnsureInteger(data: DoubleArray, factor = 1) {
   if (factor === 1) return data.map((value) => Math.round(value));
-  return xDivide(data, factor);
+  return xDivide(data, factor).map((value) => Math.round(value));
 }
